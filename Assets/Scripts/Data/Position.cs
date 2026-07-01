d8f22b849c0545e4195e0e79dc0fb37da735e59d
gusenga thierry <t.gusenga@alustudent.com>
2026-07-01 16:35:53 +0200

    Create Position.cs

---
+++ Assets/Scripts/Data/Position.cs
@@ -0,0 +1,11 @@
+namespace RestApiJson.Data
+{
+    // 3D coordinate matching the JSON "position" object.
+    [System.Serializable]
+    public class Position
+    {
+        public float x;
+        public float y;
+        public float z;
+    }
+}
